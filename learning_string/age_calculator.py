f52fc0e8bf99d2194404c0115f4d2a3b4cb7f923
subathra14 <subathra.nagarajan1@gmail.com>
2023-12-12 23:15:35 +0100

    1st change

--- learning_string/age_calculator.py
+++ learning_string/age_calculator.py
@@ -1,4 +1,6 @@
+name = input("What is your name? ")
+print("Hi "+ str(name))
 birth_year = input("Enter your birth year : ")
 print("the birth year you entered is : ", birth_year)
 age =  2023 - int(birth_year)
-print("your Age is : " , age)
+print(str(name) + "," + " your Age is : " , int(age))
